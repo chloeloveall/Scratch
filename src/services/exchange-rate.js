a03dc86ccfcdb872b7e529b0a9afd262614793af
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-12 14:21:58 -0800

    Add async function to call exchange rate api, include try...catch block for error handling

--- src/services/exchange-rate.js
+++ src/services/exchange-rate.js
@@ -1,3 +1,13 @@
 export default class ExchangeRate {
-  
+  static async getExchangeRate() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/usd`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
 }
