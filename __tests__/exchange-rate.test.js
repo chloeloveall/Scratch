a4aa5402793ce85b1fac91d22a6ac222248911a1
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-12 10:27:07 -0800

    Pass test to instantiate exchangeRate object, set up beforeEach()

---
+++ __tests__/exchange-rate.test.js
@@ -0,0 +1,17 @@
+import ExchangeRate from './../src/services/exchange-rate.js';
+
+describe('ExchangeRate', () => {
+  let exchangeRate;
+
+  beforeEach(() => {
+    exchangeRate = new ExchangeRate(100);
+  });
+
+  test('should correctly create an exchangeRate object', () => {
+    expect(exchangeRate).toBeInstanceOf(ExchangeRate);
+  });
+
+  // test('should correctly create a exchangeRate object with inputtedCurrency property', () => {
+  //   expect(exchangeRate.inputtedCurrency).toEqual(100);
+  // });
+});
