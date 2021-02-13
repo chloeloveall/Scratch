0f22d7bca3fd352a0ec02c4e51a496e11f2821f2
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-12 16:58:58 -0800

    End of day commit for Epicodus - broken code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 /*eslint-disable*/
-import $ from 'jquery';
-/*eslint-disable*/
+import $, { get } from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRate from './services/exchange-rate.js';
+/*eslint-disable*/
+
+// function clearFields() {
+//   $('#dollar-input').val("");
+//   $('#currency-input').val("");
+// }
+
+$(document).ready(function() {
+  $('#currencyForm').submit(function(event) {
+    event.preventDefault();
+    // clearFields();
+    const userDollarInput = $('#dollar-input').val();
+    // const userCurrencyInput = $('#currency-input').val();
+    (async function () {
+      const response = await ExchangeRate.getExchangeRate(userDollarInput);
+      if (!response.ok) {
+        console.log('hi');
+        $('#currencyOutput').html('<p>No results found. Please try again.</p>');
+      } else {
+        const currencyReturn = (response.conversion_rates.EUR * userDollarInput);
+        $('#currencyOutput').html(`${currencyReturn}`);
+      }
+    })
+  })
+})
+
+
